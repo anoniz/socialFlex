394713132bbdbb8db11103b83da02f57764ba950
niz <abdullatifnizamani517@gmail.com>
2024-01-26 16:21:58 +0500

    adding post image and videos

---
+++ fileUpload/stream.js
@@ -0,0 +1,24 @@
+const fs = require('fs');
+
+const http = require('http');
+
+// const file = fs.createWriteStream('./big.file');
+
+// for(let i=0; i<= 1e6 *2; i++) {
+//   file.write('Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n');
+// }
+
+// file.end();
+
+
+const server = http.createServer();
+
+server.on('request', (req,res) => {
+    const src = fs.createReadStream('./big.file');
+    src.pipe(res);
+})
+
+server.listen(8000);
+
+
+
